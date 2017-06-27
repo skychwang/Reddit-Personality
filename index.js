cd9676b3deda187c3d6a9f6d691cf389cabffe88
skywang329 <sky.wang@yahoo.com>
2017-06-27 15:07:07 +0800

    favicon redirect and cleaned up code

--- index.js
+++ index.js
@@ -34,38 +34,44 @@ mongoClient.open(function(err, mongoClient) {
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/favicon.ico', function(req, res) {
+    res.send(204);
+});
+
 app.get('/:user', function(req, res) { 
   var posts = new Array();
   request('https://www.reddit.com/user/' + req.params.user + '/.json?limit=100000', function (error, response, body) {
-    //console.log('error:', error); // Print the error if one occurred 
     //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
     //console.log('body:', body); // Print the HTML for the Google homepage. 
-    //res.set('Content-Type','application/json');
-    var data = JSON.parse(body).data.children;
-    //res.send(200, body);
-    for(var i = 0; i < data.length; i++) {
-      var obj = data[i];
-      //console.log(obj.data.body);
-      posts.push({
-        id: obj.data.id,
-        content: obj.data.body,
-        language: 'en',
-        created: obj.data.created_utc
+    if (error) {
+      res.send(error);
+    } else {
+      var postdata = JSON.parse(body).data.children;
+      for(var i = 0; i < postdata.length; i++) {
+        posts.push({
+          id: postdata[i].data.id,
+          //id: postdata[i].data.link_id,
+          contenttype: 'text/plain',
+          content: postdata[i].data.body,
+          language: 'en',
+          //reply: 
+          created: postdata[i].data.created_utc,
+          parentid: postdata[i].data.parent_id
+        });
+      };
+      personality_insights.profile({
+        content_items: posts,
+        consumption_preferences: true
+      },
+      function (err, response) {
+        if (err) {
+          res.send(err);
+        } else {
+          res.set('Content-Type','application/json');
+          res.send(response);
+        }
       });
     }
-    //console.log(posts);
-    personality_insights.profile({
-      content_items: posts,
-      consumption_preferences: true
-    },
-    function (err, response) {
-      if (err) {
-        res.send(err);
-      } else {
-        console.log(JSON.stringify(response, null, 2));
-        res.send(JSON.stringify(response, null, 2));
-      }
-    });
   });
 });
 
